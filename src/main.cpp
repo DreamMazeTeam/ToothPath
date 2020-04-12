1c399d8ade27ecd6ec90f308f09965d11c3826ea
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-04-12 22:56:20 +0300

    DBG

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,23 @@
 #define __DEBUG__ 1
 
 #if __DEBUG__ == 1
+
     #define DBG_SPEED 9600
     #define LOG(x) Serial.print((x))
-    #define LOGLN(x) LOG(x)
+    #define LOGLN(x) Serial.println((x))
+    #define DBG_INIT(x) Serial.begin((x))
+
+#else
+
+    #define LOG(x) 
+    #define LOGLN(x) 
+    #define DBG_INIT(x) 
+
 #endif
 
 void setup() 
 {
-    #ifdef __DEBUG__ == 1
-
-    Serial.begin(9600)
-
-    #endif
+    DBG_INIT(DBG_SPEED);
 }
 
 void loop()
