df1b413aca83f4bcd4110ee12bb240d122658e6d
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-04-13 23:50:58 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+
+#include "Adafruit_INA219.h"
+
+
+#define __DEBUG__ 1
+
+
+#if __DEBUG__ == 1
+
+    #define DBG_SPEED                  9600
+    #define LOG(x)        Serial.print((x))
+    #define LOGLN(x)    Serial.println((x))
+    #define DBG_INIT(x)   Serial.begin((x))
+
+#else
+
+    #define LOG(x) 
+    #define LOGLN(x) 
+    #define DBG_INIT(x) 
+
+#endif
+
+#define LEFT                 0
+#define RIGHT                1
+#define UP                   1
+#define DOWN                 0
+
+#define R_ENGINE_PIN         3
+#define L_ENGINE_PIN         5
+
+#define RED_LED_PIN          6
+#define YELLOW_LED_PIN       7
+#define GREEN_LED_PIN        8
+
+#define Y_FLASK_PIN         12
+#define X_FLASK_PIN         10
+
+#define BTN_UPPER_PIN   PIN_A1
+#define BTN_LOWWER_PIN  PIN_A0
+
+#define BATTERY_PIN     PIN_A2
+
+#define SENSETIVITY        700
+#define ENGINE_TICK_DELAY  100
+
+Adafruit_INA219 ina219(0x40);
+
+enum class Direction{ No, Left, Right, _count };
+Direction EngineDirection = Direction::No;
+
+int xFlask   = 0;
+int yFlask   = 0;
+int btnState = 0;
+
+void setEngineDirection(Direction dir)
+{
+    LOG("Engines in pos -> ");
+
+    switch (dir)
+    {
+    case Direction::Left:
+        digitalWrite(L_ENGINE_PIN, HIGH);
+        digitalWrite(R_ENGINE_PIN, LOW);
+        LOGLN("Left");
+        break;
+
+    case Direction::Right:
+        digitalWrite(L_ENGINE_PIN, LOW);
+        digitalWrite(R_ENGINE_PIN, HIGH);
+        LOGLN("Right");
+        break;
+
+    case Direction::No:
+        digitalWrite(L_ENGINE_PIN, LOW);
+        digitalWrite(R_ENGINE_PIN, LOW);
+        LOGLN("No");
+        break;
+    }
+}
+
+Direction getDirectionFromFlasks()
+{
+    if (btnState)
+    {
+        if (xFlask == LEFT && yFlask == DOWN) {
+            return Direction::Left;
+        }
+        else if (xFlask == LEFT && yFlask == UP) {
+            return Direction::Right;
+        }
+        else if (xFlask == RIGHT && yFlask == DOWN) {
+            return Direction::Right;
+        }
+        else if (xFlask == RIGHT && yFlask == UP) {
+            return Direction::Left;
+        }
+    }
+    else
+    {
+        if (xFlask == LEFT && yFlask == DOWN) {
+            return Direction::Right;
+        }
+        else if (xFlask == LEFT && yFlask == UP) {
+            return Direction::Left;
+        }
+        else if (xFlask == RIGHT && yFlask == DOWN) {
+            return Direction::Left;
+        }
+        else if (xFlask == RIGHT && yFlask == UP) {
+            return Direction::Right;
+        }
+    }
+
+    return Direction::No;
+}
+
+uint32_t engines_timer = 0;
+
+int xFlaskPrev = xFlask;
+int yFlaskPrev = yFlask;
+
+void engines_tick()
+{
+    int x = digitalRead(X_FLASK_PIN);
+    int y = digitalRead(Y_FLASK_PIN);
+
+    if ((millis() - engines_timer) >= ENGINE_TICK_DELAY)
+    {
+        if (xFlaskPrev == x)
+            xFlask = x;
+        if (yFlaskPrev == y)
+            yFlask = y;
+
+        engines_timer = millis();
+    }
+
+    xFlaskPrev = x;
+    yFlaskPrev = y;
+}
+
+void setup() 
+{
+    DBG_INIT(DBG_SPEED);
+    ina219.begin();
+
+    pinMode(RED_LED_PIN, OUTPUT);
+    pinMode(GREEN_LED_PIN, OUTPUT);
+    pinMode(YELLOW_LED_PIN, OUTPUT);
+
+    pinMode(Y_FLASK_PIN-1, OUTPUT);
+    pinMode(X_FLASK_PIN-1, OUTPUT);
+
+    digitalWrite(Y_FLASK_PIN-1, HIGH);
+    digitalWrite(X_FLASK_PIN-1, HIGH);
+}
+
+
+
+void loop()
+{   
+    engines_tick();
+
+    EngineDirection = getDirectionFromFlasks();
+    setEngineDirection(EngineDirection);
+}
